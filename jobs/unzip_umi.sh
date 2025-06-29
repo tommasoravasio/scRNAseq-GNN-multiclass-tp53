a449ee6c78a76ab2ad848891735458a4bc291eeb
Tommaso Ravasio <tommyravam@gmail.com>
2025-06-29 15:01:52 +0200

    added jobs directory and unzip_umi jobs

---
+++ jobs/unzip_umi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=unzip_umi
+#SBATCH --output=unzip_umi.out
+#SBATCH --mem=2G
+#SBATCH --time=02:00:00
+#SBATCH --partition=defq
+
+cd /home/3192281/tp53_mul/data/GSE157220
+
+echo "Unzipping UMIcount_data.txt.gz..."
+gunzip UMIcount_data.txt.gz
+echo "Done!"
